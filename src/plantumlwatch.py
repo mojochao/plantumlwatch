b50bafc0eaf97e6f484af940dd1534fd17aeee14
Allen Gooch <egoochiv@zynga.com>
2015-07-23 12:05:53 -0500

    Added new configure command.

--- src/plantumlwatch.py
+++ src/plantumlwatch.py
@@ -21,14 +21,18 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
-CONFIGURATION_FILE_NAME = '.plantumlwatch'
-
 DESCRIPTION = """
-Watches a directory for changes to PlantUML source files with a *.pu
-extension and generate corresponding UML diagram image files, either in PNG
-or SVG format, when modified.
+Watches a directory for changes to PlantUML source files and generates
+corresponding UML diagram image files on modification.  COMMAND may be
+one of 'watch' or 'configure'.  The 'watch' command begins watching
+for source file modifications.  The 'configure' command generates a
+configuration file that may be used instead of providing options on
+the command line.  If no COMMAND is provided, the default is 'watch'.
 """
 
+CONFIG_FILENAME = '.plantumlwatch'
+
+DEFAULT_CONFIG_FILE = os.path.join(os.getcwd(), CONFIG_FILENAME)
 DEFAULT_VERBOSE = False
 DEFAULT_JAVA = "java"
 DEFAULT_PLANTUML = "plantuml.jar"
@@ -37,25 +41,26 @@ DEFAULT_WATCH_EXTENSION = "pu"
 DEFAULT_OUTPUT_DIRECTORY = os.getcwd()
 DEFAULT_OUTPUT_FORMAT = "png"
 
-DEFAULT_CONFIGURATION = {
+DEFAULT_CONFIG = {
     "verbose": DEFAULT_VERBOSE,
     "java": DEFAULT_JAVA,
     "plantuml": DEFAULT_PLANTUML,
-    "watchdir": os.getcwd(),
+    "watchdir": DEFAULT_WATCH_DIRECTORY,
     "extension": DEFAULT_WATCH_EXTENSION,
-    "outputdir": os.getcwd(),
+    "outputdir": DEFAULT_OUTPUT_DIRECTORY,
     "format": DEFAULT_OUTPUT_FORMAT
 }
 
+VALID_COMMANDS = ["watch", "configure"]
 VALID_OUTPUT_FORMATS = ["png", "svg"]
 
 
-def configuration(argopts):
+def configuration(options):
     """Configuration object builder.
 
     Returns configuration object built from multiple configuration sources.
 
-    A sample JSON configuration object might look like the following:
+    A sample JSON configuration object looks like the following:
 
         {
             "verbose": false,
@@ -67,49 +72,44 @@ def configuration(argopts):
             "format": "png"
         }
 
-    The configuration object is built by successively loading these values from:
-    1. the default object
-    2. the object deserialized from user's home .plantumlwatch file, if exists
-    3. the object deserialized from .plantumlwatch file in current working directory, if exists
-    4. the object built from command line options
+    The configuration object is built by loading these values from one of:
+    1. the object deserialized from .plantumlwatch file in current working directory, if exists
+    2. the object built from command line options and their defaults
 
-    :param argopts: arguments passed on command line
-    :type argopts: dict
+    :param options: options passed on command line
+    :type options: dict
 
     :returns: app configuration
     :rtype: dict
 
     """
-    config = dict(DEFAULT_CONFIGURATION)
+    filename = options['config']
+    if not os.path.isfile(filename):
+        return options
 
-    config_filenames = [
-        os.path.expanduser("~/{}".format(CONFIGURATION_FILE_NAME)),
-        os.path.abspath("./{}".format(CONFIGURATION_FILE_NAME))
-    ]
-    for filename in config_filenames:
-        if os.path.isfile(filename):
-            try:
-                with open(filename) as infile:
-                    global_configuration = json.load(infile)
-                config.update(global_configuration)
-            except ValueError as err:
-                raise RuntimeError("configuration file {} is not a valid json file: {}".format(filename, err))
+    try:
+        with open(filename) as infile:
+            return json.load(infile)
+    except ValueError as err:
+        raise RuntimeError("configuration file '{}' is not a valid json file: {}".format(filename, err))
 
-    if argopts is not None:
-        config.update(argopts)
 
-    return config
+def configure(self):
+    """Writes out a configuration file in current working directory."""
+    if os.path.exists(CONFIG_FILENAME):
+        print("configuration file exists: {}")
+        return
+
+    with open(CONFIG_FILENAME, "w") as outfile:
+        json.dump(outfile, DEFAULT_CONFIG)
+    print("wrote configuration file: {}".format(os.path.abspath(CONFIG_FILENAME)))
 
 
 class WatcherFileChangeHandler(FileSystemEventHandler):
-    """Handler class for PlantUML source file modifications.
-
-    This handler generates UML diagrams on PlantUML source file modifications.
-
-    """
+    """Handler class generating diagrams for PlantUML source file modifications."""
 
     def __init__(self, watcher):
-        """Initialize new PlantUmlFileHandler object.
+        """Initializes new PlantUmlFileHandler object.
 
         :param watcher: Watcher associated with this file modification handler
         :type watcher: :class:`plantumlwatch.Watcher`
@@ -167,16 +167,27 @@ class Watcher(object):
         Watchers are responsible for configuring themselves, and when run
         watching those files for changes and generating new output.
 
+        The *config* dict looks like the following:
+
+        {
+            "verbose": false,
+            "java": "/usr/local/bin/java",
+            "plantuml": "~/lib/plantuml.8024.jar",
+            "watchdir": ".",
+            "extension": "*.pu",
+            "outputdir": "~/myoutputdir",
+            "format": "png"
+        }
+
         :param config: configuration object
         :type config: dict
 
         """
-
         self._configuration = config
         if not os.path.isfile(self.java):
-            raise RuntimeError("cannot find Java Virtual Machine binary '{}'".format(self.java))
+            raise RuntimeError("cannot find Java Virtual Machine binary file '{}'".format(self.java))
         if not os.path.isfile(self.plantuml):
-            raise RuntimeError("cannot find PlantUML Jar '{}'".format(self.plantuml))
+            raise RuntimeError("cannot find PlantUML jar file '{}'".format(self.plantuml))
         if not os.path.isdir(self.watchdir):
             raise RuntimeError("cannot find watch directory '{}'".format(self.watchdir))
         if not os.path.isdir(self.outputdir):
@@ -186,35 +197,34 @@ class Watcher(object):
 
     @property
     def verbose(self):
-        return self._configuration["verbose"]
+        return self._configuration["verbose"] if "verbose" in self._configuration else DEFAULT_VERBOSE
 
     @property
     def java(self):
-        return self._configuration["java"]
+        return self._configuration["java"] if "java" in self._configuration else DEFAULT_JAVA
 
     @property
     def plantuml(self):
-        return self._configuration["plantuml"]
+        return self._configuration["plantuml"] if "plantuml" in self._configuration else DEFAULT_PLANTUML
 
     @property
     def watchdir(self):
-        return self._configuration["watchdir"]
+        return self._configuration["watchdir"] if "watchdir" in self._configuration else DEFAULT_WATCH_DIRECTORY
 
     @property
     def extension(self):
-        return self._configuration["extension"]
+        return self._configuration["extension"] if "extension" in self._configuration else DEFAULT_WATCH_EXTENSION
 
     @property
     def outputdir(self):
-        return self._configuration["outputdir"]
+        return self._configuration["outputdir"] if "outputdir" in self._configuration else DEFAULT_OUTPUT_DIRECTORY
 
     @property
     def format(self):
-        return self._configuration["format"]
+        return self._configuration["format"] if "format" in self._configuration else DEFAULT_OUTPUT_FORMAT
 
-    def run(self):
+    def watch(self):
         """Watch PlantUML files in configuration directories for modifications."""
-
         # Let the user know what's up.
         print("watching directory {} for files with extension '{}'".format(self.watchdir, self.extension))
         if self.verbose:
@@ -237,42 +247,53 @@ def main():
     """main entrypoint."""
 
     # Configure the arguments parser and parse them from the command line arguments.
-    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser = argparse.ArgumentParser(usage="%(prog)s [OPTIONS] COMMAND", description=DESCRIPTION)
+    parser.add_argument("-c", "--config",
+                        default=DEFAULT_CONFIG_FILE,
+                        help="configuration file to use. defaults to '{}'.".format(DEFAULT_CONFIG_FILE),
+                        metavar="FILE")
     parser.add_argument("-v", "--verbose",
                         action="store_true",
-                        help="enable verbose output")
+                        help="enable verbose output. defaults to '{}'.".format(DEFAULT_VERBOSE))
     parser.add_argument("-j", "--java",
                         default=DEFAULT_JAVA,
-                        help="java jvm to use. defaults to 'java' in your path",
-                        metavar="JVM")
+                        help="java jvm to use. defaults to '{}'.".format(DEFAULT_JAVA),
+                        metavar="FILE")
     parser.add_argument("-p", "--plantuml",
                         default=DEFAULT_PLANTUML,
-                        help="plantuml jarfile to use. defaults to 'plantuml.jar' in your current working directory",
-                        metavar="JAR")
+                        help="plantuml jarfile to use. defaults to '{}'.".format(DEFAULT_PLANTUML),
+                        metavar="FILE")
     parser.add_argument("-w", "--watchdir",
                         default=DEFAULT_WATCH_DIRECTORY,
-                        help="directory to watch",
+                        help="directory to watch. defaults to current working directory.",
                         metavar="PATH")
     parser.add_argument("-e", "--extension",
                         default=DEFAULT_WATCH_EXTENSION,
-                        help="extension of files to watch",
+                        help="extension of files to watch. defaults to '{}'.".format(DEFAULT_WATCH_EXTENSION),
                         metavar="PATH")
     parser.add_argument("-o", "--outputdir",
                         default=DEFAULT_OUTPUT_DIRECTORY,
-                        help="directory to write output to",
+                        help="directory to write output to. defaults to current working directory.",
                         metavar="PATH")
     parser.add_argument("-f", "--format",
                         default=DEFAULT_OUTPUT_FORMAT,
                         choices=VALID_OUTPUT_FORMATS,
-                        help="output format, either 'png' or 'svg'. defaults to 'png'",
+                        help="output format to emit. valid values include {}. defaults to '{}'.".format(VALID_OUTPUT_FORMATS, DEFAULT_OUTPUT_FORMAT),
                         metavar="FORMAT")
-    args = vars(parser.parse_args())
+    args, commands = parser.parse_known_args()
+    if not commands:
+        commands = ["watch"]
 
     # Configure and run the watcher.
     try:
-        config = configuration(args)
-        watcher = Watcher(config)
-        watcher.run()
+        if not commands or len(commands) > 1 or commands[0] not in VALID_COMMANDS:
+            parser.print_help()
+        elif commands[0] == "watch":
+            config = configuration(vars(args))
+            watcher = Watcher(config)
+            watcher.watch()
+        elif commands[0] == "configure":
+            configure()
     except Exception as err:
         sys.exit("error: {}".format(err))
 
